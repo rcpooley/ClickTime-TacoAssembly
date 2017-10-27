b606215e100e1abe017ec67d9e2ff8ef91ecd446
Robert Pooley <rcpooley@gatech.edu>
2017-10-27 03:04:07 -0400

    Worked on sentence generation

--- taco-app/src/app/services/taco.service.ts
+++ taco-app/src/app/services/taco.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, EventEmitter} from "@angular/core";
-import {Ingredient} from "./ingredient.service";
+import {Ingredient, IngredientService} from "./ingredient.service";
 
 export interface Taco {
     ingredients: {[s: string]: Ingredient[]};
@@ -11,24 +11,83 @@ export interface TacoData {
     tacos: Taco[];
 }
 
+interface SentenceTemplate {
+    base: string;
+    opts: {[s: string]: string};
+    max: number;
+}
+
 @Injectable()
 export class TacoService {
 
     data: TacoData;
 
+    sentenceTemplates: SentenceTemplate[];
+
     newTaco: EventEmitter<boolean>;
     nextTacoId: number;
 
-    constructor() {
+    constructor(private ingredientService: IngredientService) {
         this.data = {
             tacos: []
         };
         this.newTaco = new EventEmitter<boolean>();
         this.nextTacoId = 1;
+
+        this.sentenceTemplates = [
+            {
+                base: 'Your taco is served in a %shells%, ' +
+                'formed on the base layer of %baseLayers%, ' +
+                'with a dash of %mixins%%mixins1%, ' +
+                'complimented with a delicious choice of condiments featuring %allcondiments%, ' +
+                'and to top it all off is seasoned with %seasonings%',
+                opts: {
+                    mixins1: ' and a pinch of %mixins%'
+                },
+                max: 3
+            }
+        ];
     }
 
-    private static setTacoSentence(taco: Taco): void {
-        taco.sentence = "lol";
+    private setTacoSentence(taco: Taco): void {
+        let getOnlyIng = function (cat: string) {
+            return taco.ingredients[cat][0].name;
+        };
+
+        let temp: SentenceTemplate = this.sentenceTemplates[Math.floor(Math.random() * this.sentenceTemplates.length)];
+
+        let bld: string = temp.base;
+
+        let repl = ['shells', 'baseLayers', 'mixins', 'condiments', 'seasonings'];
+        for (let i = 0; i < repl.length; i++) {
+            bld = bld.replace(new RegExp('%' + repl[i] + '%',"g"), getOnlyIng(repl[i]));
+
+            let ings = taco.ingredients[repl[i]];
+            for (let j = 1; j < temp.max; j++) {
+                let key = repl[i] + j;
+                let val = '';
+                if (key in temp.opts && j < ings.length) {
+                    val = temp.opts[key].replace(new RegExp('%' + repl[i] + '%',"g"), ings[j].name);
+                }
+                bld = bld.replace(new RegExp('%' + key + '%',"g"), val);
+            }
+        }
+
+        let condiments = taco.ingredients['condiments'];
+        let conStr = condiments[0].name;
+        for (let i = 1; i < condiments.length; i++) {
+            if (i == condiments.length - 1) {
+                if (i > 1) {
+                    conStr += ',';
+                }
+                conStr += ' and ' + condiments[i].name;
+            } else {
+                conStr += ', ' + condiments[i].name;
+            }
+        }
+        bld = bld.replace(/%allcondiments%/g, conStr);
+
+        taco.sentence = bld;
     }
 
     getNewTacoName(): string {
@@ -36,7 +95,7 @@ export class TacoService {
     }
 
     addTaco(taco: Taco): void {
-        TacoService.setTacoSentence(taco);
+        this.setTacoSentence(taco);
         this.data.tacos.push(taco);
         this.newTaco.emit(true);
     }
